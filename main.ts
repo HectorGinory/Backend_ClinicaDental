afdbae3bdd8ad545156b33ed45e9b1311315695f
alex65cast <alex65578@gmail.com>
2023-04-21 17:46:02 +0200

    Client functions with handlerError

--- main.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const app = express();
-app.use(express.json());
-
-app.listen(3000, () => console.log("Servidor levantado en 3000"));
-
-// listado descripciones no
-// detalles descripciones si
