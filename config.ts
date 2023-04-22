62bb5b1d8d2fdc51a8017cf4381d53826d174764
HectorGinory <hectorginorydevs@gmail.com>
2023-04-22 17:11:00 +0200

    Alexdev merged to dev

--- config.ts
+++ config.ts
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 const config = {
     SECRET: process.env.SECRET,
     SALT_ROUNDS: process.env.SALT_ROUNDS,
     DDBB: process.env.DDBB
 }
 
-=======
-const config = {
-    SECRET: process.env.SECRET,
-    SALT_ROUNDS: process.env.SALT_ROUNDS,
-    DDBB: process.env.DDBB
-}
-
->>>>>>> afdbae3bdd8ad545156b33ed45e9b1311315695f
 export default config
