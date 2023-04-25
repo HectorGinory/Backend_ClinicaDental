3808adc610e51ab1191e1f107022965defd5be53
alex65cast <alex65578@gmail.com>
2023-04-25 17:52:58 +0200

    Creation controllers and routers1_0

--- dist/entities/user/model.js
+++ dist/entities/user/model.js
@@ -1,2 +1,29 @@
-export {};
+import mongoose from "mongoose";
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        min: 3,
+        max: 100
+    },
+    email: {
+        type: String,
+        required: true,
+        min: 3,
+        max: 100,
+        unique: true
+    },
+    password: {
+        type: String,
+        select: false,
+        required: true,
+        min: 6
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+}, { versionKey: false });
+const Users = mongoose.model('Users', UserSchema);
+export default Users;
 //# sourceMappingURL=model.js.map
